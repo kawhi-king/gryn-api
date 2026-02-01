33ad3b82a0de2f18f56cb95e238d2e2de8cd4ab8
teddy yinda <teddynetha1@gmail.com>
2026-02-02 00:16:13 +0100

    feature: added login feature, with users seed

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use Illuminate\Validation\ValidationException;
 
-Route::get('/login', UserController::class . '@userIndex');
+Route::get('/test', UserController::class . '@userIndex')->middleware('auth:sanctum');
 
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required|min',
+    ]);
 
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ["L'email ou le mot de passe est incorrect."],
+        ]);
+    }
+
+    return $user->createToken($request->email)->toJson();
+});
